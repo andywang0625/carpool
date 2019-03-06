77f6efc516a818c29ef51d33dc258d263b5f628c
TC_CHEN <tchen117@uottawa.ca>
2019-03-05 19:43:03 -0500

    initialize

---
+++ src/main/java/com/web/carpool/model/Comment.java
@@ -0,0 +1,12 @@
+package com.web.carpool.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "comment")
+public class Comment {
+    @Id
+    
+}
