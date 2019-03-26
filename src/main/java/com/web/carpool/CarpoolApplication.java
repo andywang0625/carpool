0e4bc047f2b2a21f385787fdee5549896e9e70ec
Liang-Chu <45859181+Liang-Chu@users.noreply.github.com>
2019-03-26 08:16:20 -0400

    Revert "feature(rank): rank crud is able to run"

--- src/main/java/com/web/carpool/CarpoolApplication.java
+++ src/main/java/com/web/carpool/CarpoolApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class CarpoolApplication {
 
 	public static void main(String[] args) {
