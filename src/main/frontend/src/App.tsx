e669072c24987336a0d9068a740b31a069c38f55
bumblebees1111 <gu000031@algonquinlive.com>
2019-07-20 14:30:09 -0400

    feat: add navigator set routes

--- src/main/frontend/src/App.tsx
+++ src/main/frontend/src/App.tsx
@@ -1,32 +1,16 @@
 import * as React from 'react';
-import { compose } from 'recompose';
-import { Theme, createStyles, withStyles, WithStyles, Typography } from '@material-ui/core';
-import { UserForm } from './components/registration/UserForm';
+import { BrowserRouter } from 'react-router-dom';
+import { AppRoutes } from './AppRoutes';
 
-export type StyleKeys = 'root';
-
-export interface AppProps { }
-
-export interface AppBaseProps extends WithStyles<StyleKeys> { }
-
-class AppBase extends React.Component<AppBaseProps> {
+export class App extends React.Component<{}> {
 
   render = () => {
 
-    const { classes } = this.props;
-
-    return <UserForm></UserForm>;
+    return (
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    );
   }
 
 }
-
-export const App: React.ComponentType<AppProps> = compose<AppBaseProps, AppProps>(
-  withStyles(
-    (theme: Theme) => createStyles<StyleKeys>({
-      root: {
-        color: theme.palette.primary.main,
-        fontSize: theme.typography.h1.fontSize,
-      },
-    }),
-  ),
-)(AppBase);
---
+++ src/main/frontend/src/AppRoutes.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { Route, RouteComponentProps, Switch, withRouter } from 'react-router';
+import { compose } from 'recompose';
+
+interface AppRoutesProps { }
+
+interface AppRoutesBaseProps extends RouteComponentProps { }
+
+class AppRoutesBase extends React.PureComponent<AppRoutesBaseProps> {
+  render = () => (
+    <Switch>
+      <Route
+        path='/'
+        component={}
+        exaxt
+      />
+      <Route
+        path='/login'
+        component={}
+      />
+      <Route
+        path='signup'
+        component={}
+      />
+    </Switch>
+  )
+}
+
+export const AppRoutes = compose<AppRoutesBaseProps, AppRoutesProps>(
+  withRouter
+)(AppRoutesBase);
